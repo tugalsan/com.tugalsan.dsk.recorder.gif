b51ecdeac3ad1796e5719de0cc1e6c8449338dfc
tugalsan karabacak <tugalsan@gmail.com>
2023-07-26 08:26:47 +0300

    .

--- src/main/java/com/tugalsan/trm/recorder/gif/Main.java
+++ src/main/java/com/tugalsan/trm/recorder/gif/Main.java
@@ -1,17 +1,14 @@
 package com.tugalsan.trm.recorder.gif;
 
+import com.tugalsan.api.desktop.server.*;
 import com.tugalsan.trm.recorder.gif.core.*;
-import javax.swing.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        //ffmpeg -f gif -i infile.gif outfile.mp4
+    public static volatile TS_FileGifCoreGUI gui;
 
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-        }
-        SwingUtilities.invokeLater(() -> TS_FileGifCoreGUI.of());
+    //TODO ffmpeg -f gif -i infile.gif outfile.mp4
+    public static void main(String[] args) {
+        TS_DesktopMainUtils.setThemeAndinvokeLaterAndFixTheme(() -> gui = TS_FileGifCoreGUI.of());
     }
 }
