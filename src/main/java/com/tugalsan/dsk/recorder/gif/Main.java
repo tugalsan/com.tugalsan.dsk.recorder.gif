88b7b2f36f0245cc17c21d6030c5a76fc41da337
tugalsan karabacak <tugalsan@gmail.com>
2023-07-30 08:25:16 +0300

    .

--- src/main/java/com/tugalsan/dsk/recorder/gif/Main.java
+++ src/main/java/com/tugalsan/dsk/recorder/gif/Main.java
@@ -3,6 +3,9 @@ package com.tugalsan.dsk.recorder.gif;
 import com.tugalsan.api.desktop.server.*;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
+//WHEN RUNNING IN NETBEANS, ALL DEPENDENCIES SHOULD HAVE TARGET FOLDER!
+//cd C:\me\codes\com.tugalsan\dsk\com.tugalsan.dsk.recorder.gif
+//java --enable-preview --add-modules jdk.incubator.concurrent -jar target/com.tugalsan.dsk.recorder.gif-1.0-SNAPSHOT-jar-with-dependencies.jar
 public class Main {
 
     public static volatile GUI gui;
