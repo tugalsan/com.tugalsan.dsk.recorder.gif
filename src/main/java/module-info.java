4a42e25e6aee0c8580c7b8b6ed46b7510f81eae4
tugalsan karabacak <tugalsan@gmail.com>
2023-07-30 08:30:28 +0300

    .

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.tugalsan.dsk.recorder.gif {
+    requires java.desktop;
+    requires com.tugalsan.api.unsafe;
+    requires com.tugalsan.api.runnable;
+    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.desktop;
+    requires com.tugalsan.api.file.gif;
+    requires com.tugalsan.api.thread;
+}
